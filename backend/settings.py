e3151b46cee2f413ca8914b6f134d99be11bcb6a
ochogwuprince92 <ochogwuprince92@gmail.com>
2025-09-28 11:46:26 +0100

    Update Workflows

--- backend/settings.py
+++ backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 # ---------------------
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -157,6 +158,7 @@ STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_ROOT = BASE_DIR / "media"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # ---------------------
 # Other settings
